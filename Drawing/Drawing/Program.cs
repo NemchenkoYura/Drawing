1c54b8c95c5605db59ced946191cf9a1eaeefebe
NemchenkoYura <Zoroo3721@gmail.com>
2022-10-29 14:01:24 +0300

    Create Drawing

---
+++ Drawing/Drawing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("_");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Drawing/yg/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace yg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("_");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
